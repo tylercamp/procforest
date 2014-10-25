818b3a0f1f8278e5665fe3bd8390ec4c7d664746
Tyler Camp <tyler.camp@loop.colum.edu>
2014-10-25 02:25:32 -0500

    Fixed particle size scaling

--- resources/shaders/particle-vertex.glsl
+++ resources/shaders/particle-vertex.glsl
@@ -1,10 +1,15 @@
 
+//  https://www.cs.uaf.edu/2009/spring/cs480/lecture/02_03_pretty.html
+
 uniform mat4 u_ModelViewMatrix;
 uniform mat4 u_ProjectionMatrix;
 
 attribute vec4 a_Vertex;
 
+const float ParticleSize = 5.0;
+
 void main(void) {
     gl_Position = u_ProjectionMatrix * u_ModelViewMatrix * a_Vertex;
-    gl_PointSize = 1.0/gl_Position.z;
+
+    gl_PointSize = ParticleSize / gl_Position.w;
 }
