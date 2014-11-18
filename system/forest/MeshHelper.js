18af6ae81c963424245e6288e9fc61e81236b3ae
Tyler Camp <tyler.camp@loop.colum.edu>
2014-11-18 15:49:22 -0600

    Small performance optimizations

--- system/forest/MeshHelper.js
+++ system/forest/MeshHelper.js
@@ -18,16 +18,25 @@
         basePoint.y += normal.y * offset_;
         basePoint.z += normal.z * offset_;
 
-        var perpVector, rotationMatrix, result;
+        var perpVector, rotationMatrix, rotationNormal, rotationElements;
         perpVector = Math.normal(Math.perpVector(normal));
         perpVector = new Vector4([perpVector.x, perpVector.y, perpVector.z, 1]);
 
         rotationMatrix = new Matrix4().setRotate(angleDegrees, normal.x, normal.y, normal.z);
-        result = rotationMatrix.multiplyVector4(perpVector);
-        result = Math.normal({ x: result.elements[0], y: result.elements[1], z: result.elements[2] });
-        result = Math.vecSum(Math.vecMultiply(result, rotationalDistance), basePoint);
+        rotationNormal = rotationMatrix.multiplyVector4(perpVector);
+        rotationElements = rotationNormal.elements;
 
-        return result;
+        var len = Math.sqrt(
+            rotationElements[0]*rotationElements[0] +
+            rotationElements[1]*rotationElements[1] +
+            rotationElements[2]*rotationElements[2]
+        );
+
+        return {
+            x: basePoint.x + (rotationElements[0] * rotationalDistance) / len,
+            y: basePoint.y + (rotationElements[1] * rotationalDistance) / len,
+            z: basePoint.z + (rotationElements[2] * rotationalDistance) / len
+        };
     };
 
     window.MeshHelper = MeshHelper;
